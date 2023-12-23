3f744c2ee8d3864fcade16f1cdb200121982d6d6
youssefelalfy <yossefelalfy74@gmaol.com>
2023-12-23 18:59:26 +0200

    + extends WidgetsBindingObserver in HomeController + Import 'dart:math' + Import 'flutter/widgets.dart' + Import 'geolocator/geolocator.dart' + Implement onInit method with WidgetsBinding.instance.addObserver and adeterminePositionAndUpdateBranch() + add didChangeAppLifecycleState() Method + add onClose() Method + Add _determinePosition method + Add _setNearestBranch method + Add determinePositionAndUpdateBranch method + Add setSelectedBranchToDefault method + Add _calculateDistance method

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,3 +1,7 @@
+import 'dart:math';
+
+import 'package:flutter/widgets.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 
@@ -13,7 +17,7 @@ import '../../../data/repository/item_repo.dart';
 import '../../../data/repository/my_order_repo.dart';
 import '../../../data/repository/popular_item_repo.dart';
 
-class HomeController extends GetxController {
+class HomeController extends GetxController with WidgetsBindingObserver {
   static Server server = Server();
   List<OrdersData> activeOrderData = <OrdersData>[];
   List<CategoryData> categoryDataList = <CategoryData>[];
@@ -45,6 +49,22 @@ class HomeController extends GetxController {
       getActiveOrderList();
     }
     super.onInit();
+    WidgetsBinding.instance.addObserver(this);
+    determinePositionAndUpdateBranch();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+    if (state == AppLifecycleState.resumed) {
+      determinePositionAndUpdateBranch();
+    }
+  }
+
+  @override
+  void onClose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.onClose();
   }
 
   setIndexOfBranch() {
@@ -133,5 +153,87 @@ class HomeController extends GetxController {
       activeOrderData = activeOrder.data!;
       update();
     } else {}
+  }
+
+  determinePositionAndUpdateBranch() async {
+    Position? currentPosition = await _determinePosition();
+    if (currentPosition != null) {
+      _setNearestBranch(currentPosition);
+    } else {
+      setSelectedBranchToDefault();
+    }
+  }
+
+  Future<Position?> _determinePosition() async {
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return null;
+    }
+
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return null;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return null;
+    }
+
+    return await Geolocator.getCurrentPosition();
+  }
+
+  void _setNearestBranch(Position currentPosition) {
+    double minDistance = double.infinity;
+    BranchData? nearestBranch;
+
+    for (var branch in branchDataList) {
+      double distance = _calculateDistance(
+        currentPosition.latitude,
+        currentPosition.longitude,
+        double.parse(branch.latitude!),
+        double.parse(branch.longitude!),
+      );
+      if (distance < minDistance) {
+        minDistance = distance;
+        nearestBranch = branch;
+      }
+    }
+
+    if (nearestBranch != null) {
+      selectedBranch = nearestBranch.name;
+      selectedbranchId = nearestBranch.id;
+      selectedBranchIndex = branchDataList.indexOf(nearestBranch);
+
+      print(
+          "User's Current Location: Latitude: ${currentPosition.latitude}, Longitude: ${currentPosition.longitude}");
+      print(
+          "Nearest Branch:${nearestBranch.name}, Location: Latitude: ${nearestBranch.latitude}, Longitude: ${nearestBranch.longitude}");
+
+      update();
+    }
+  }
+
+  double _calculateDistance(
+      double lat1, double lon1, double lat2, double lon2) {
+    var p = 0.017453292519943295;
+    var c = cos;
+    var a = 0.5 -
+        c((lat2 - lat1) * p) / 2 +
+        c(lat1 * p) * c(lat2 * p) * (1 - c((lon2 - lon1) * p)) / 2;
+    return 12742 * asin(sqrt(a));
+  }
+
+  void setSelectedBranchToDefault() {
+    if (branchDataList.isNotEmpty) {
+      selectedBranch = branchDataList[0].name;
+      selectedbranchId = branchDataList[0].id;
+      selectedBranchIndex = 0;
+      update();
+    } else {
+      print("Branch data list is empty");
+    }
   }
 }
